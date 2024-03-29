3d3e8d9c48d80dd3abb59114e2471dbdd84947d6
Ljubomir Ilievski <ilievski.ljubomir@gmail.com>
2024-03-29 20:22:47 +0100

    Call method makeBigStringFrom and filterOddStrings in main class

--- SILab1_2.java
+++ SILab1_2.java
@@ -2,7 +2,7 @@ import java.util.List;
 
 public class SILab1_2 {
 
-    private static String makeBigStringFrom (List<String> list, int minLength) {
+    public static String makeBigStringFrom (List<String> list, int minLength) {
         StringBuilder sb = new StringBuilder();
         for (String el : list) {
             if (el.length() >= minLength) {
